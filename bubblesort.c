56ccc6591ce2bbaa03a8cb365aa8f027c6fd774d
gasolineeater <ezekaj@42.heilbronn.de>
2025-03-14 21:03:49 +0100

    Bubble sort algorithm.

--- bubblesort.c
+++ bubblesort.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+
+void	bubblesort(int arr[], int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	j = 0;
+	temp = arr[j];
+	while (i < size)
+	{
+		while (j < size - i - 1)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
