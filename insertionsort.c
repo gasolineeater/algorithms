a44701add70e58521ad9f7007b5144be4552b9ad
gasolineeater <ezekaj@42.heilbronn.de>
2025-03-19 09:26:56 +0100

    ADD: insetion sort algorithm

--- insertionsort.c
+++ insertionsort.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 
+void	insertionsort(int arr[], int n)
+{
+	int	i;
+	int	tmp;
+	int	j;
 
+	i = 1;
+	while (i < n)
+	{
+		tmp = arr[i];
+		j = i - 1;
+		while(tmp < arr[j] && j >= 0)
+		{
+			arr[j + 1] = arr[j];
+			--j;
+		}
+		arr[j + 1] = tmp;
+		i++;
+	}
+}
